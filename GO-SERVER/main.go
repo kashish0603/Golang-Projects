1f6f1d0a023700e46d73539ea40252d19b2fb7a8
kashish0603 <kashish.jain2021a@vitstudent.ac.in>
2024-04-26 19:16:26 +0530

    Basic Web Server with form

---
+++ GO-SERVER/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "methis is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Email = %s\n", email)
+
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static")) //will directly access the index.html file just like other servers like php
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil { //ListenAndServe will create the server, really the heart of the software we are creating
+		log.Fatal(err)
+	}
+}
